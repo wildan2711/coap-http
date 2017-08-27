618cff34e93473094fb4dd4d7592c0124b31a721
Wildan Maulana Syahidillah <wildan2711@gmail.com>
2017-08-27 21:35:45 +0700

    initial commit

---
+++ coapserver.js
@@ -0,0 +1,33 @@
+var coap        = require('coap')
+var http        = require('http')
+var request     = require('request')
+var coapServer  = coap.createServer()
+
+var httpServerIP = "192.168.100.26"
+var httpServerPort = "8080"
+
+var suhu = 0
+var lembab = 0
+
+coapServer.on('request', function(req, res) {
+    var route = req.url.split('/')[1]
+    var payload = JSON.parse(req.payload) 
+    if (route == "monitor") {
+        suhu = payload.suhu
+        lembab = payload.lembab
+
+        console.log("suhu:",suhu)
+        console.log("lembab:",lembab)
+        var url = httpServerIP + ':' + httpServerPort
+                    +'/monitor?suhu='
+                    +suhu
+                    +"?lembab="
+                    +lembab
+        request.get(url)
+    }
+})
+
+// the default CoAP port is 5683
+coapServer.listen(function() {
+    console.log("Server listening on port 5683")
+})
---
+++ httpserver.js
@@ -0,0 +1,6 @@
+var http = require('http')
+var port = "8080"
+
+http.createServer(function(req, res) {
+    console.log(req)
+})
